96669874c2cb2eee013bdccd9dda001ff15ca409
Abhijeet Ranmale <abhijeet.ranmale@perennialsys.com>
2023-08-29 13:34:30 +0530

    Added repo list and details page

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <p className="text-center">Loading....</p>
+    </div>
+  );
+}
+
+export default Loader;
---
+++ src/pages/SingleRepoDetails/SingleRepo.js
@@ -0,0 +1,108 @@
+import gql from "graphql-tag";
+import React from "react";
+import { Query } from "react-apollo";
+import { useParams } from "react-router-dom";
+import Loader from "../../components/Loader";
+import ErrorComponent from "../../components/ErrorComponent";
+
+const SingleRepo = () => {
+  let { repo_name } = useParams();
+
+  return (
+    <Query
+      query={gql`
+        query Query {
+          repoScannerDetails(repoName: "${repo_name}"){
+            name
+            size
+            private
+            active_webhook
+            yml_content
+            total_files
+            owner{
+              id
+              login
+              avatar_url
+              url
+            }
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <Loader />;
+        if (error) return <ErrorComponent message={error.message} />;
+        return (
+          <>
+            <div className="container mt-5">
+              <h1 className="bg-primary text-white p-3 text-center">
+                Repository Details
+              </h1>
+              <div className="container mt-5">
+                <div className="border rounded p-3">
+                  <h4>The repo details are as follows:</h4>
+                  <table className="table table-bordered">
+                    <tbody className="bg-info text-white col-3">
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          Repository Name
+                        </th>
+                        <td>{data.repoScannerDetails.name}</td>
+                      </tr>
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          Owner Username
+                        </th>
+                        <td>{data.repoScannerDetails.owner.login}</td>
+                      </tr>
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          Is Repository Private
+                        </th>
+                        <td>
+                          {data?.repoScannerDetails?.private ? "Yes" : "No"}
+                        </td>
+                      </tr>
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          Repository Size
+                        </th>
+                        <td>{data.repoScannerDetails.size}</td>
+                      </tr>
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          Active Webhooks
+                        </th>
+                        <td>
+                          {data.repoScannerDetails.active_webhook
+                            ? "Yes"
+                            : "No"}
+                        </td>
+                      </tr>
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          Total Files in Repo
+                        </th>
+                        <td>{data.repoScannerDetails.total_files}</td>
+                      </tr>
+                      <tr>
+                        <th className="bg-info text-white col-3">
+                          YML Content
+                        </th>
+                        <td>
+                          {data.repoScannerDetails.yml_content ? "" : "null"}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      }}
+    </Query>
+  );
+};
+
+export default SingleRepo;
